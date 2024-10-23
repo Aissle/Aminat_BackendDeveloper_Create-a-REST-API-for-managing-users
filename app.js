283eb02e20af3a9048a2de87159d4e7860369d04
Idris Aminat <idris.oaminat@gmail.com>
2024-10-23 13:52:39 +0100

    Handle Routes
    Import and use authRoutes.js and userRoutes.js on app.js

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 
 app.use(express.json());
-mongoose.connect('mongodb://localhost:27017/userdb', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/mngurs", {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.listen(3000, () => {
-    console.log('This server is running on http://localhost:3000');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use("", authRoutes);
+app.use("", userRoutes); 
+
+app.listen(6000, () => {
+    console.log('This server is running on http://localhost:6000');
 });
+
