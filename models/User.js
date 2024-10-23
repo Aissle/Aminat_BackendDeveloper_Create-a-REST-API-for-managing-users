45ae9796549826988507b77233518040a7270881
Idris Aminat <idris.oaminat@gmail.com>
2024-10-23 15:04:13 +0100

    Resolve issues and fix errors

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: { type: String, required: true, unique: true},
-    password: {type: string, required: true},
-    role: {type: string, default: 'User', enum: ['User', 'Admin']}
+    password: {type: String, required: true},
+    role: {type: String, default: 'User', enum: ['User', 'Admin']}
 });
 
-const user = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
